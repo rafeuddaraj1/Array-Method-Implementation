aca12761606177f02a8346ba00366aac24e78c7d
rafeuddaraj <rafeuddaraj2@gmail.com>
2021-08-30 21:19:53 +0600

    Function declartion reduce method

--- Reduce.js
+++ Reduce.js
@@ -1,1 +1,8 @@
 // Reduce Method Implementation
+
+function reduce(arr, callBack, accumulator) {
+    for (let i = 0; i < arr.length; i++){
+        accumulator = callBack(accumulator,arr[i],i,arr)
+    }
+    return accumulator
+}
